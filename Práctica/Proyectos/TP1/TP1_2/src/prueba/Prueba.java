5b208a1d0495c19f160b0a0f48ae95674a8f8d25
santilapi13 <santilapiana02@gmail.com>
2022-03-12 19:42:06 -0300

    TP1_2 Terminado

---
+++ Práctica/Proyectos/TP1/TP1_2/src/prueba/Prueba.java
@@ -0,0 +1,44 @@
+package prueba;
+
+public class Prueba {
+	public static void main(String[] args) {
+		/*
+		int min;
+		Persona p1 = new Persona();
+		Persona p2 = new Persona();
+		Persona p3 = new Persona();
+		Lugar l1 = new Lugar();
+		Lugar l2 = new Lugar();
+		Lugar l3 = new Lugar();
+		Imagen i1 = new Imagen();
+		Imagen i2 = new Imagen();
+		Imagen i3 = new Imagen();
+		int edadP1 = p1.getEdad(),edadP2 = p2.getEdad( ),edadP3 = p3.getEdad();
+		
+		i1.etiquetarPersona(p1);
+		i1.etiquetarPersona(p2);
+		
+		i3.agregarComentario("que hermosa foto");
+		
+		min = edadP1;
+		if (edadP2 < min)
+			min = edadP2;
+		if (edadP3 < min)
+			min = edadP3;
+		System.out.println("Edad minima: " + min);
+		
+		if (i2.getPropietario() == p3)
+			System.out.println("p3 es propietario de i2");
+		else
+			System.out.println("p3 no es propietario de i2");
+		
+		String cadena = new String(i3.getComentarios());
+		System.out.println(cadena);
+		
+		String paisAuxiliar = new String(i2.getLugar().getPais());
+		System.out.println(paisAuxiliar);
+		
+		i1.setLugar(l2);
+		*/
+	}
+}

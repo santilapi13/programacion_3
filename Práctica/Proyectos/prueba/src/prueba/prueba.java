654446cb49c69d5dc168cf3b43a2d496326b7cc0
Santi <santilapiana02@gmail.com>
2022-04-14 13:37:12 -0300

    Actualizado

---
+++ Práctica/Proyectos/prueba/src/prueba/prueba.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class prueba {
+
+	public prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int inst, a;
+		inst = 10;
+		a = inst << 2;
+		System.out.println(inst);
+	}
+
+}
